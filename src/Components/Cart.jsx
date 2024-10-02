bcaa2f51e39607e5949a7269f1631f4c610f606d
Shivamani-ragipani <shivamaniragipani@gmail.com>
2024-10-02 20:53:13 +0530

    Submit Order Button

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -27,7 +27,7 @@ export default function Cart() {
     <Modal
       className="cart"
       open={userProgressCtx.progress === "cart"}
-      onClose={userProgressCtx.progress === "cart" && handlehideCart}
+      onClose={userProgressCtx.progress === "cart" ? handlehideCart : null}
     >
       <h2>Your Cart</h2>
       <ul>
--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -20,13 +20,35 @@ export default function Checkout() {
     userProgressCtx.hideCart();
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+
+    fetch("http://localhost:5000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      }),
+    });
+  }
   return (
-    <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleCloseCart}>
-      <form>
+    <Modal
+      open={userProgressCtx.progress === "checkout"}
+      onClose={handleCloseCart}
+    >
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {CurrencyFormatter.format(carttotal)}</p>
 
-        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="Full Name" type="text" id="name" />
         <Input label="E-Mail" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
         <div className="control-row">
